a4b2cc794e87ede7f27945550a6741818f77061a
Sergey Bernyakovich <s.bernyakovich@kmtf.kz>
2022-04-19 15:21:12 +0500

    разработан navbar

--- src/app/layouts/Main.jsx
+++ src/app/layouts/Main.jsx
@@ -1,19 +1,22 @@
 import React from 'react'
 import { Switch, Route } from 'react-router-dom'
-import Navbar from '../components/ui/navbar'
 import AboutPage from '../pages/about-page'
 import SingleImagePage from '../pages/single-image-page'
 import GallaryPage from '../pages/gallary-page'
+import { Container } from 'react-bootstrap'
+import NavbarComponent from '../components/ui/navbar-component'
 
 const Main = () => {
 	return (
 		<>
-			<Navbar />
-			<Switch>
-				<Route path='/about' component={AboutPage} />
-				<Route path='/:photoId' component={SingleImagePage} />
-				<Route path='/' component={GallaryPage} />
-			</Switch>
+			<NavbarComponent />
+			<Container className='mt-3'>
+				<Switch>
+					<Route path='/about' component={AboutPage} />
+					<Route path='/:photoId' component={SingleImagePage} />
+					<Route path='/' component={GallaryPage} />
+				</Switch>
+			</Container>
 		</>
 	)
 }
