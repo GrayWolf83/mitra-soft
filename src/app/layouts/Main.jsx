4f9ffd59c881ef76b56427dfdce4403c3977de78
Sergey Bernyakovich <s.bernyakovich@kmtf.kz>
2022-04-19 13:34:51 +0500

    Реализована маршрутизация

--- src/app/layouts/Main.jsx
+++ src/app/layouts/Main.jsx
@@ -1,7 +1,21 @@
 import React from 'react'
+import { Switch, Route } from 'react-router-dom'
+import Navbar from '../components/ui/navbar'
+import AboutPage from '../pages/about-page'
+import SingleImagePage from '../pages/single-image-page'
+import GallaryPage from '../pages/gallary-page'
 
 const Main = () => {
-	return <h1>Main</h1>
+	return (
+		<>
+			<Navbar />
+			<Switch>
+				<Route path='/about' component={AboutPage} />
+				<Route path='/:photoId' component={SingleImagePage} />
+				<Route path='/' component={GallaryPage} />
+			</Switch>
+		</>
+	)
 }
 
 export default Main
